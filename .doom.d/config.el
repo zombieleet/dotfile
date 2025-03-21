1fc019d172586b5dcaaaaffe463c09b5487b82d6
Victory Osikwemhe <osikwemhev@gmail.com>
2025-03-21 23:32:05 +0100

    Add files via upload

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,11 +6,11 @@
 (setq-default tab-width 2)
 
 (setq-default tab-stop-list
-   '(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60))
+              '(2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60))
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Victory"
+      user-mail-address "osikwemhev@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -44,7 +44,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-(setq-default js-indent-level 2)
+(setq-default js-indent-level 2)
 (setq-default typescript-indent-level 2)
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -78,13 +78,330 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-(use-package! zig-mode
-  :hook ((zig-mode . lsp-deferred))
-  :custom (zig-format-on-save nil)
+(defun copy-current-buffer-file-name ()
+  (interactive)
+  (shell-command (concat "echo " (buffer-file-name) " | pbcopy")))
+
+;; Go mode configuration
+(use-package! go-mode
+  :defer t)
+
+;; Flycheck
+(use-package! flycheck
+  :ensure t)
+
+;; LSP configuration for Go
+(use-package! lsp-mode
+  :hook ((go-mode . lsp-deferred)
+         (js2-mode . lsp-deferred))
+  :commands (lsp lsp-deferred))
+
+;; JS2 Mode for JavaScript
+(use-package! js2-mode
+  :defer t)
+
+;; Node.js REPL
+(use-package! nodejs-repl
+  :ensure t)
+
+;; Company for autocompletion
+(use-package! company
+  :ensure t)
+
+;; LSP Treemacs integration
+(use-package! lsp-treemacs
+  :after (lsp-mode treemacs)
+  :ensure t)
+
+
+;; DAP mode configuration
+(use-package! dap-mode
+  :after lsp-mode
   :config
-  (after! lsp-mode
-    (add-to-list 'lsp-language-id-configuration '(zig-mode . "zig"))
-    (lsp-register-client
-      (make-lsp-client
-        :major-modes '(zig-mode)
-        :server-id 'zls))))
+  (dap-auto-configure-mode)
+
+  ;; Go debugging configuration
+  (require 'dap-dlv-go)
+
+  ;; Node.js debugging configuration
+  (require 'dap-node)
+  (dap-node-setup)
+
+  ;; Add environment variables for TypeScript tests
+  (setq dap-node-default-debug-program "node")
+
+  ;; Direct function for debugging TypeScript tests
+  (defun dap-debug-typescript-test ()
+    "Debug current TypeScript test file with proper configuration."
+    (interactive)
+    (let* ((project-dir (projectile-project-root))
+           (file-name (buffer-file-name))
+           (rel-file (file-relative-name file-name project-dir)))
+      (dap-debug
+       (list :type "node"
+             :request "launch"
+             :name "Debug TypeScript Test"
+             :runtimeExecutable "node"
+             :runtimeArgs (vector "--inspect-brk"
+                                  (concat project-dir "node_modules/.bin/jest")
+                                  "--runInBand"
+                                  "--no-cache"
+                                  rel-file)
+             :cwd project-dir
+             :env (list (cons "TS_NODE_PROJECT" (concat project-dir "tsconfig.test.base.json"))
+                        (cons "NODE_ENV" "test")
+                        (cons "JEST_TEST_TYPE" "unit"))
+             :sourceMaps t
+             :console "integratedTerminal"
+             :internalConsoleOptions "neverOpen"
+             :skipFiles (vector "<node_internals>/**")))))
+
+  ;; Enhance all Node.js launch configurations for TypeScript
+  (advice-add 'dap-register-debug-template :around
+              (lambda (orig-fun name conf)
+                "Enhance TypeScript test configurations in launch.json"
+                ;; Check if this is a Node.js configuration that might involve TypeScript
+                (when (and (plist-get conf :type)
+                           (string= (plist-get conf :type) "node")
+                           (or (and (plist-get conf :program)
+                                    (string-match-p "jest\\|ts-node" (plist-get conf :program)))
+                               (and (plist-get conf :runtimeArgs)
+                                    (seq-some (lambda (arg)
+                                                (string-match-p "jest\\|ts-node\\|typescript" arg))
+                                              (plist-get conf :runtimeArgs)))))
+                  ;; This is likely a TypeScript-related configuration, enhance it
+                  (setq conf (plist-put conf :sourceMaps t))
+
+                  ;; Add TypeScript environment variables if not already present
+                  (let ((env (plist-get conf :env)))
+                    (unless (assoc "TS_NODE_PROJECT" env)
+                      (setq env (cons (cons "TS_NODE_PROJECT" "${workspaceFolder}/tsconfig.test.base.json") env)))
+                    (unless (assoc "NODE_ENV" env)
+                      (setq env (cons (cons "NODE_ENV" "test") env)))
+                    (unless (assoc "JEST_TEST_TYPE" env)
+                      (setq env (cons (cons "JEST_TEST_TYPE" "unit") env)))
+                    (plist-put conf :env env)))
+
+                ;; Call the original function with possibly enhanced configuration
+                (funcall orig-fun name conf)))
+
+  ;; Add keybinding for the direct debug function
+  (map! :leader
+        (:prefix "d"
+         :desc "Debug TypeScript test" "t" #'dap-debug-typescript-test))
+
+  ;; Function to load the .vscode/launch.json file and set up DAP configurations
+  (defun dap-load-vscode-launch-json ()
+    "Load the launch.json file from .vscode directory and setup dap-mode."
+    (interactive)
+    (let* ((project-dir (or (projectile-project-root) default-directory))
+           (launch-file (concat project-dir ".vscode/launch.json")))
+      (if (file-exists-p launch-file)
+          (progn
+            (message "Loading %s" launch-file)
+            (with-temp-buffer
+              (insert-file-contents launch-file)
+              ;; Remove comments if they exist (VSCode supports JSON with comments)
+              (goto-char (point-min))
+              (while (re-search-forward "\\(//.*\\)" nil t)
+                (replace-match ""))
+              ;; Also handle multi-line comments
+              (goto-char (point-min))
+              (while (re-search-forward "/\\*\\(.\\|\n\\)*?\\*/" nil t)
+                (replace-match ""))
+              ;; Remove trailing commas which are invalid in standard JSON
+              (goto-char (point-min))
+              (while (re-search-forward ",[ \t\n]*[]}]" nil t)
+                (replace-match "\\1"))
+              ;; Now parse the cleaned JSON
+              (goto-char (point-min))
+              (condition-case err
+                  (let ((json-object-type 'hash-table)
+                        (json-array-type 'list)
+                        (json-key-type 'string)
+                        (json-false nil)
+                        (json-null nil))
+                    (let ((launch-config (json-read)))
+                      (when-let ((configurations (gethash "configurations" launch-config)))
+                        (dolist (config configurations)
+                          (let ((config-type (gethash "type" config)))
+                            (cond
+                             ((string= config-type "go")
+                              ;; Convert VSCode Go config to DAP Go config
+                              (dap-register-debug-template
+                               (gethash "name" config)
+                               (list :type "go"
+                                     :request (gethash "request" config)
+                                     :name (gethash "name" config)
+                                     :mode (or (gethash "mode" config) "auto")
+                                     :program (gethash "program" config)
+                                     :args (or (gethash "args" config) nil)
+                                     :env (or (gethash "env" config) nil)
+                                     :cwd (or (gethash "cwd" config) "${workspaceFolder}"))))
+
+                             ((string= config-type "node")
+                              ;; Convert VSCode Node config to DAP Node config
+                              (dap-register-debug-template
+                               (gethash "name" config)
+                               (list :type "node"
+                                     :request (gethash "request" config)
+                                     :name (gethash "name" config)
+                                     :program (gethash "program" config)
+                                     :args (or (gethash "args" config) nil)
+                                     :env (or (gethash "env" config) nil)
+                                     :cwd (or (gethash "cwd" config) "${workspaceFolder}")
+                                     :sourceMaps (or (gethash "sourceMaps" config) t)
+                                     :protocol (or (gethash "protocol" config) "inspector")
+                                     :restart (or (gethash "restart" config) nil)
+                                     :port (or (gethash "port" config) 9229))))))))))
+                (error
+                 (message "Error parsing launch.json: %s" (error-message-string err))
+                 nil)))
+            (message "Launch configurations loaded from %s" launch-file))
+        (message "No .vscode/launch.json found in project"))))
+
+
+  ;; Add hooks for Go and JS modes to load launch.json
+  (add-hook 'go-mode-hook #'dap-load-vscode-launch-json)
+  (add-hook 'js2-mode-hook #'dap-load-vscode-launch-json)
+
+  ;; Enable Dap UI features
+  (dap-ui-mode 1)
+  (dap-tooltip-mode 1)
+  (tooltip-mode 1)
+
+  ;; Corrected extended keybindings for DAP mode
+  (map! :leader
+        (:prefix "d" ; debug
+         ;; Core debugging commands
+         :desc "Start debugging" "d" #'dap-debug
+         :desc "Debug last configuration" "l" #'dap-debug-last
+         :desc "Debug TypeScript test" "T" #'dap-debug-typescript-test
+         :desc "Continue" "c" #'dap-continue
+         :desc "Next" "n" #'dap-next
+         :desc "Step in" "i" #'dap-step-in
+         :desc "Step out" "o" #'dap-step-out
+         :desc "Restart frame" "r" #'dap-restart-frame
+         :desc "Disconnect" "q" #'dap-disconnect
+
+         ;; Simple breakpoint toggle remains at top level
+         :desc "Toggle breakpoint" "b" #'dap-breakpoint-toggle
+
+         ;; Breakpoint management under "B" prefix
+         (:prefix ("B" . "breakpoints")
+          :desc "Add breakpoint" "a" #'dap-breakpoint-add
+          :desc "Delete breakpoint" "d" #'dap-breakpoint-delete
+          :desc "Delete all breakpoints" "D" #'dap-breakpoint-delete-all
+          :desc "Toggle condition" "c" #'dap-breakpoint-condition
+          :desc "Toggle hit condition" "h" #'dap-breakpoint-hit-condition
+          :desc "Toggle log message" "l" #'dap-breakpoint-log-message)
+
+         ;; Information and state inspection
+         (:prefix ("I" . "information")
+          :desc "Show locals" "l" #'dap-ui-locals
+          :desc "Show breakpoints" "b" #'dap-ui-breakpoints
+          :desc "Show expressions" "e" #'dap-ui-expressions
+          :desc "Show sessions" "s" #'dap-ui-sessions
+          :desc "Show all windows" "a" #'dap-ui-show-many-windows
+          :desc "List templates" "t" #'dap-debug-last-configuration
+          :desc "Eval at point" "p" #'dap-eval-thing-at-point
+          :desc "Eval in repl" "r" #'dap-ui-repl)
+
+         ;; Stack frame navigation
+         (:prefix ("F" . "frames")
+          :desc "Next frame" "n" #'dap-next-frame
+          :desc "Previous frame" "p" #'dap-previous-frame
+          :desc "Switch frame" "s" #'dap-switch-stack-frame
+          :desc "Up frame" "u" #'dap-up-stack-frame
+          :desc "Down frame" "d" #'dap-down-stack-frame)
+
+         ;; Expression evaluation
+         (:prefix ("E" . "evaluate")
+          :desc "Eval" "e" #'dap-eval
+          :desc "Eval region" "r" #'dap-eval-region
+          :desc "Eval thing at point" "t" #'dap-eval-thing-at-point
+          :desc "Add expression" "a" #'dap-ui-expressions-add)))
+
+  ;; Add hook to ensure UI elements are available
+  (add-hook 'dap-stopped-hook
+            (lambda (arg)
+              (call-interactively #'dap-hydra)))
+
+  ;; Custom function to start debugging with configuration selection
+  (defun my/dap-debug-select ()
+    "Start debugging with configuration selection."
+    (interactive)
+    (call-interactively #'dap-debug))
+
+  (map! :leader
+        (:prefix "d"
+         :desc "Debug (select config)" "s" #'my/dap-debug-select))
+
+  :hook ((go-mode . dap-mode)
+         (js2-mode . dap-mode)))
+
+;; Enable DAP mode by default in programming modes
+(add-hook 'prog-mode-hook #'dap-mode)
+
+;; If you want the UI controls to also be enabled by default
+(add-hook 'prog-mode-hook #'dap-ui-mode)
+
+;; Ellama configuration
+(use-package ellama
+  :ensure t
+  :bind ("C-c e" . ellama-transient-main-menu)
+  ;; send last message in chat buffer with C-c C-c
+  :hook (org-ctrl-c-ctrl-c-final . ellama-chat-send-last-message)
+  :init
+  ;; setup key bindings
+  ;; (setopt ellama-keymap-prefix "C-c e")
+  ;; language you want ellama to translate to
+  (setopt ellama-language "English")
+  ;; could be llm-openai for example
+  (require 'llm-ollama)
+  (setopt ellama-provider
+          (make-llm-ollama
+           ;; this model should be pulled to use it
+           ;; value should be the same as you print in terminal during pull
+           :chat-model "qwen2.5-coder:14b-instruct-q5_K_M"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("num_ctx" . 8192))))
+  (setopt ellama-summarization-provider
+          (make-llm-ollama
+           :chat-model "qwen2.5:14b-instruct-q5_K_M"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("num_ctx" . 32768))))
+  (setopt ellama-coding-provider
+          (make-llm-ollama
+           :chat-model "qwen2.5-coder:14b-instruct-q5_K_M"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("num_ctx" . 32768))))
+  ;; Naming new sessions with llm
+  (setopt ellama-naming-provider
+          (make-llm-ollama
+           :chat-model "llama3:8b-instruct-q5_K_M"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params '(("stop" . ("\n")))))
+  (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)
+  ;; Translation llm provider
+  (setopt ellama-translation-provider
+          (make-llm-ollama
+           :chat-model "qwen2.5-coder:14b-instruct-q5_K_M"
+           :embedding-model "nomic-embed-text"
+           :default-chat-non-standard-params
+           '(("num_ctx" . 32768))))
+  (setopt ellama-extraction-provider (make-llm-ollama
+                                      :chat-model "qwen2.5-coder:7b-instruct-q8_0"
+                                      :embedding-model "nomic-embed-text"
+                                      :default-chat-non-standard-params
+                                      '(("num_ctx" . 32768))))
+  ;; customize display buffer behaviour
+  ;; see ~(info "(elisp) Buffer Display Action Functions")~
+  (setopt ellama-chat-display-action-function #'display-buffer-full-frame)
+  (setopt ellama-instant-display-action-function #'display-buffer-at-bottom)
+  :config
+  ;; show ellama context in header line in all buffers
+  (ellama-context-header-line-global-mode +1)
+  ;; handle scrolling events
+  (advice-add 'pixel-scroll-precision :before #'ellama-disable-scroll)
+  (advice-add 'end-of-buffer :after #'ellama-enable-scroll))
--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,34 +3,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-ui-doc-alignment 'frame)
- '(lsp-ui-doc-delay 0.5)
- '(lsp-ui-doc-enable t)
- '(lsp-ui-doc-enhanced-markdown t)
- '(lsp-ui-doc-header t)
- '(lsp-ui-doc-include-signature t)
- '(lsp-ui-doc-max-height 100)
- '(lsp-ui-doc-max-width 100)
- '(lsp-ui-doc-position 'at-point)
- '(lsp-ui-doc-show-with-cursor t)
- '(lsp-ui-doc-show-with-mouse t)
- '(lsp-ui-doc-use-webkit t)
- '(lsp-ui-doc-webkit-max-width-px 1000)
- '(lsp-ui-imenu-auto-refresh t)
- '(lsp-ui-imenu-window-fix-width t)
- '(lsp-ui-peek-always-show t)
- '(lsp-ui-peek-enable t)
- '(lsp-ui-sideline-actions-icon
-   "/Users/victoryosikwemhe/.emacs.d/.local/straight/build-27.2/lsp-ui/resources/lightbulb.png")
- '(lsp-ui-sideline-ignore-duplicate t)
- '(lsp-ui-sideline-show-code-actions t)
- '(lsp-ui-sideline-show-hover t)
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(company-quickhelp-terminal company-quickhelp lsp-docker py-autopep8 python-black pylint markdown-preview-mode markdown-mode markdown-preview-eww markdownfmt pandoc-mode pandoc read-aloud rfc-mode tree-sitter yaml-pro flymake-yamllint magithub docker-compose-mode org-preview-html terraform-doc terraform-mode prettier go-playground protobuf-mode sproto-mode git-link inf-mongo mongo lsp-grammarly zig-mode hcl-mode phps-mode google-this grammarly graphql-doc graphql-mode graphql attrap js-doc)))
+   '(terraform-mode dap-mode protobuf-mode k8s-mode git-link makefile-executor kubernetes jest-test-mode restclient)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(lsp-ui-doc-background ((t (:extend t :stipple nil :background "black" :foreground "black" :box (:line-width 10 :color "grey75" :style released-button))))))
+ )
 (put 'customize-group 'disabled nil)
+
+(require 'jest-test-mode)
+(add-hook 'typescript-mode-hook 'jest-test-mode)
+(add-hook 'js-mode-hook 'jest-test-mode)
+(add-hook 'typescript-tsx-mode-hook 'jest-test-mode)
